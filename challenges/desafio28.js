1afff7dc7914482e974ffc8c4406b640e43c4fa2
Roberval Filho <rslfilho@gmail.com>
2021-10-04 19:42:42 -0300

    Desafio 28

---
+++ challenges/desafio28.js
@@ -0,0 +1,10 @@
+db.resumoVoos.insertOne({
+  empresa: "LATAM AIRLINES BRASIL",
+  totalVoosDomesticos: 20026,
+});
+
+db.resumoVoos.findOne({
+  empresa: "LATAM AIRLINES BRASIL",
+}, {
+  _id: 0,
+});
