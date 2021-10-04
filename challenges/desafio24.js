519400ddc130a972403e6f885932e1d587df5b26
Roberval Filho <rslfilho@gmail.com>
2021-10-04 19:42:20 -0300

    Desafio 24

---
+++ challenges/desafio24.js
@@ -0,0 +1,11 @@
+db.voos.findOne({
+  $and: [
+    { litrosCombustivel: { $lte: 600, $exists: true } },
+    { "empresa.nome": { $nin: ["GOL", "AZUL"] } },
+  ],
+}, {
+  _id: 0,
+  vooId: 1,
+  "empresa.nome": 1,
+  litrosCombustivel: 1,
+});
