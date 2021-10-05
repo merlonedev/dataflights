2928252230c7afdc3747f8e404bbe1afb5244be6
alestori7@gmail.com <alestori7@gmail.com>
2021-10-05 00:01:24 -0300

    Desafio 22 :white_check_mark:

---
+++ challenges/desafio22.js
@@ -0,0 +1,10 @@
+db.voos.findOne(
+  {
+    "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] },
+    $and: [
+      { "aeroportoOrigem.sigla": "SBGR" },
+      { "aeroportoDestino.sigla": "KJFK" },
+    ],
+  },
+  { _id: 0, vooId: 1 },
+);
