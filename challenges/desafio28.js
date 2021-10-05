159b750b91ec623169582567fffa680321957367
Milton Castro <castro.milton07@gmail.com>
2021-10-05 04:00:02 -0300

    Desafio 28

---
+++ challenges/desafio28.js
@@ -0,0 +1,10 @@
+db.resumoVoos.insertOne({
+  empresa: "LATAM AIRLINES BRASIL",
+  totalVoosDomesticos: db.voos.find(
+    { "empresa.nome": "LATAM AIRLINES BRASIL", natureza: "Doméstica" },
+  ).count(),
+});
+
+db.resumoVoos.find(
+  {}, { _id: 0, empresa: 1, totalVoosDomesticos: 1 },
+).limit(1);
